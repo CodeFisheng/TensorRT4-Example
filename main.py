050b531fce16c19ba22641786b611bf21443077c
YunYang1994 <YunYang1994@github.com>
2018-11-12 16:42:56 +0800

    add file

--- main.py
+++ main.py
@@ -1,13 +1,85 @@
+import sys
 import tftensort
-import numpy as np
-from scipy import misc
-
-# model = tftensort.Trtmodel("./model/resnetv2.pb", "./data/labellist.json", "input_tensor", "softmax_tensor")
-model = tftensort.Trtmodel("./model/resnetv2-fp16.pb", "./data/labellist.json", "input_tensor", "softmax_tensor")
-# model.optimize("./model", "FP16")
-image_data = misc.imread("./data/image.jpg")
-image_data = np.tile(image_data, [1, 1, 1, 1])
-model.predict(image_data)
+import argparse
 
 
 
+def main(argv):
+
+    flags = parser(description="Parser to contain flags for running the TensorRT timers.").parse_args()
+    model = tftensort.Trtmodel(flags.frozen_graph, flags.labels_file, flags.input_node, flags.output_node)
+    # BATCH_SIZE = flags.batch_size
+
+    if flags.fp16:
+        model.optimize(flags.output_dir, mode='FP16')
+    if flags.fp32:
+        model.optimize(flags.output_dir, mode='FP32')
+    if flags.int8:
+        model.optimize(flags.output_dir, mode='INT8')
+
+    if flags.image_file is not None:
+        model.predict(flags.image_file)
+
+class parser(argparse.ArgumentParser):
+
+    def __init__(self,description):
+        super(parser, self).__init__(description)
+
+        self.add_argument(
+            "--frozen_graph", "-fg", default=None,
+            help="[default: %(default)s] The location of a Frozen Graph ",
+            metavar="<FG>",
+        )
+
+        self.add_argument(
+            "--labels_file", "-lf", default="./data/labellist.json",
+            help="[default: %(default)s] The location of a labels_file ",
+            metavar="<LF>",
+        )
+
+        self.add_argument(
+            "--output_dir", "-od", default="./model",
+            help="[default: %(default)s] The location where output files will "
+            "be saved.",
+            metavar="<OD>",
+        )
+
+        self.add_argument(
+            "--input_node", "-in", default="input_tensor",
+            help="[default: %(default)s] The name of the graph input node where "
+            "the float image array should be fed for prediction.",
+            metavar="<IN>",
+        )
+
+        self.add_argument(
+            "--output_node", "-on", default="softmax_tensor",
+            help="[default: %(default)s] The names of the graph output node "
+            "that should be used when retrieving results. Assumed to be a softmax.",
+            metavar="<ON>",
+        )
+
+        self.add_argument(
+            "--image_file", "-if", default=None,
+            help="[default: %(default)s] The location of a JPEG image that will ",
+            metavar="<IF>",
+        )
+
+        self.add_argument(
+            "--fp32", action="store_true",
+            help="[default: %(default)s] If set, benchmark the model with TensorRT "
+            "using fp32 precision."
+        )
+
+        self.add_argument(
+            "--fp16", action="store_true",
+            help="[default: %(default)s] If set, benchmark the model with TensorRT "
+            "using fp16 precision."
+        )
+
+        self.add_argument(
+            "--int8", action="store_true",
+            help="[default: %(default)s] If set, benchmark the model with TensorRT "
+            "using int8 precision."
+        )
+
+if __name__ == "__main__": main(argv=sys.argv)
--- tftensort.py
+++ tftensort.py
@@ -14,7 +14,16 @@ import tensorflow.contrib.tensorrt as trt ##  WARNNING: must load when do predic
 ################################################################################
 # Run this script
 # => Sub-Graph Optimizations within TensorFlow
+# Requirement: tensorrt version ==> '4.0.1.6'
 ################################################################################
+
+_RESIZE_MIN = 256
+_R_MEAN = 123.68 # ADJUST
+_G_MEAN = 116.78 # ADJUST
+_B_MEAN = 103.94 # ADJUST
+_CHANNEL_MEANS = [_R_MEAN, _G_MEAN, _B_MEAN]
+means = np.expand_dims(np.expand_dims(_CHANNEL_MEANS, 0), 0)
+tf.logging.set_verbosity(tf.logging.INFO)
 
 class Trtmodel(object):
     """
@@ -39,9 +48,17 @@ class Trtmodel(object):
         with open(labels_file, 'r') as labels_file:
             self.labels = json.load(labels_file)
 
-        with tf.gfile.FastGFile(self.pb_file, 'rb') as f:
-            self.frozen_graph_def = tf.GraphDef()
-            self.frozen_graph_def.ParseFromString(f.read())
+        # with tf.gfile.FastGFile(self.pb_file, 'rb') as f:
+            # self.frozen_graph_def = tf.GraphDef()
+            # self.frozen_graph_def.ParseFromString(f.read())
+        self.frozen_graph_def = self.get_frozen_graph(self.pb_file)
+
+    def get_frozen_graph(self, pb_file):
+        """Read Frozen Graph file from disk."""
+        with tf.gfile.FastGFile(pb_file, "rb") as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+        return graph_def
 
     def optimize(self, output_dir, mode):
 
@@ -54,38 +71,77 @@ class Trtmodel(object):
         assert mode in ["FP16", "FP32", "INT8"]
         graph_def = trt.create_inference_graph(input_graph_def=self.frozen_graph_def,
                                                outputs=[self.OUTPUT_NODE],
-                                               max_batch_size=128,
+                                               max_batch_size=1,
                                                max_workspace_size_bytes=1 << 32,
                                                precision_mode=mode)
 
         output_path = os.path.join(output_dir, self.pb_name+"-{}.pb".format(mode.lower()))
+
         with tf.gfile.GFile(output_path, 'wb') as f:
             f.write(graph_def.SerializeToString())
+        return graph_def
 
+    def _smallest_size_at_least(self, height, width, resize_min):
+        resize_min = tf.cast(resize_min, tf.float32)
+        height, width = tf.cast(height, tf.float32), tf.cast(width, tf.float32)
 
-    def predict(self, image_data, verbose=True):
-        """
-        Args:
-            image_data: np.ndarray, shape: [BATCH_SIZE, HEIGHT, WIDTH, CHANNELS]
-            verbose: True or False
-        """
-        if verbose: import time
+        smaller_dim = tf.minimum(height, width)
+        scale_ratio = resize_min / smaller_dim
 
-        _R_MEAN = 123.68 # ADJUST
-        _G_MEAN = 116.78 # ADJUST
-        _B_MEAN = 103.94 # ADJUST
-        _CHANNEL_MEANS = [_R_MEAN, _G_MEAN, _B_MEAN]
-        BATCH_SIZE, HEIGHT, WIDTH = image_data.shape[:3]
+        new_height = tf.cast(height * scale_ratio, tf.int32)
+        new_width = tf.cast(width * scale_ratio, tf.int32)
 
-        means = np.expand_dims(np.expand_dims(_CHANNEL_MEANS, 0), 0)
-        image_data = image_data - means
+        return new_height, new_width
+
+    def _resize_image(self, image, height, width):
+        return tf.image.resize_images(
+            image, [height, width], method=tf.image.ResizeMethod.BILINEAR, align_corners=False)
+
+    def _aspect_preserving_resize(self, image, resize_min):
+        shape = tf.shape(image)
+        height, width = shape[0], shape[1]
+
+        new_height, new_width = self._smallest_size_at_least(height, width, resize_min)
+
+        return self._resize_image(image, new_height, new_width)
+
+    def _central_crop(self, image, crop_height, crop_width):
+        shape = tf.shape(image)
+        height, width = shape[0], shape[1]
+
+        amount_to_be_cropped_h = (height - crop_height)
+        crop_top = amount_to_be_cropped_h // 2
+        amount_to_be_cropped_w = (width - crop_width)
+        crop_left = amount_to_be_cropped_w // 2
+        return tf.slice(image, [crop_top, crop_left, 0], [crop_height, crop_width, -1])
+
+    def preprocess_image(self, image_file, num_channels, output_height, output_width):
+
+        image_buffer = tf.read_file(image_file)
+        image = tf.image.decode_jpeg(image_buffer, channels=num_channels)
+        image = self._aspect_preserving_resize(image, _RESIZE_MIN)
+        print(image, output_height, output_width)
+        image = self._central_crop(image, output_height, output_width)
+
+        image.set_shape([output_height, output_width, num_channels])
+        image = image - means
+
+        with tf.Session() as sess:
+            return sess.run([image])[0]
+
+    def predict(self, image_file, verbose=True, num_loops=100):
+
+        import time
+
+        image_data = self.preprocess_image(image_file, 3, 224, 224)
+        image_data = np.tile(image_data, [1, 1, 1, 1])
 
         tf.reset_default_graph()
         graph = tf.Graph()
 
         # tf.logging.info("Starting execution")
         with graph.as_default():
-            images = tf.placeholder("float", [BATCH_SIZE, HEIGHT, WIDTH, 3])
+            images = tf.placeholder("float", [1, 224, 224, 3])
             return_tensors = tf.import_graph_def(graph_def=self.frozen_graph_def,
                                                 input_map={self.INPUT_NODE: images},
                                                 return_elements=[self.OUTPUT_NODE])
@@ -96,11 +152,23 @@ class Trtmodel(object):
             output = return_tensors[0].outputs[0]
 
         with tf.Session(graph=graph) as sess:
-            embeddings = sess.run([output], feed_dict={images:image_data})
+            tf.logging.info("Starting Warmup cycle")
+            for _ in range(10): sess.run([output], feed_dict={images: image_data})
 
-            start = time.time()
-            result = [self.labels[str(np.argmax(embedding[0]))] for embedding in embeddings]
-            if verbose: print("=> running time: {} prediction: {}".format(time.time()-start, result))
+            tf.logging.info("Starting timing.")
+            timing = []
+            for _ in range(num_loops):
+                start = time.time()
+                embeddings = sess.run([output], feed_dict={images:image_data})
+                result = [self.labels[str(np.argmax(embedding[0]))] for embedding in embeddings]
+                timing.append(time.time()-start)
+
+            tf.logging.info("Timing loop done!")
+            speed = 1/np.array(timing) # BATCH_SIZE = 1
+            t_max, t_min, t_mean, t_std = max(speed), min(speed), speed.mean(), speed.std()
+            print("=> prediction: {}".format(result))
+            print("=> Frame Per Second info: max {:.2f} fps, min {:.2f} fps, mean {:.2f} fps, std {:.2f} fps"\
+                  .format(t_max, t_min, t_mean, t_std))
 
         return result
 
